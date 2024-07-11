e3cb14fe650a788834494af6466c12fa2b406a6f
Mohammad Nazir Azizi <159978396+NazirAziz@users.noreply.github.com>
2024-07-10 21:31:45 -0700

    Update ans.cpp

--- Special Substring/ans.cpp
+++ Special Substring/ans.cpp
@@ -1,3 +1,14 @@
+/*
+Chef gave you a string 𝑆S of size 𝑁N which contains only lowercase English letters and asked you
+to find the length of the longest substring of the stringwhich satisfies the following condition:
+•	Each character 𝛽β should appear at most 𝑓(𝛽)f(β) times.
+Here 𝑓(𝛽)f(β) denotes the index of the character 𝛽β in the alphabet series. For example 𝑓(′𝑎′)f(′a′) = 11, 𝑓(′𝑏′)f(′b′) = 22 and so on.
+Note: A substring of a string is a contiguous subsequence of that string.
+
+
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 int howManyTimesShouldBeRepeated(char c){
